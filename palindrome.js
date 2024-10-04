7c57a7231a5d61f33e3d0e949070bcb9ee892e55
MicheleFumi <michele.fumi96@gmail.com>
2024-10-04 15:05:10 +0200

    add code

---
+++ palindrome.js
@@ -0,0 +1,24 @@
+/* 
+Palidroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+ */
+let message;
+let input_text =prompt("inserisci una parola per verificare la palindricità di essa")
+function isPalindrome(input_text) {
+   let text_lenght = input_text.length
+   let mid_text = Math.floor(text_lenght/2);
+   
+    for (let i = 0; i < mid; i++) {
+    
+    if (input_text[i] == input_text[text_lenght -1 -i] ){
+        message = "la parola è palindroma";
+        
+    }else {
+        message = "la parola non è palindroma";
+        
+    }
+ 
+   } 
+} 
+
+
