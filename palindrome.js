b26a2533281c0275738cde7c9195dea92fb49b5d
MicheleFumi <michele.fumi96@gmail.com>
2024-10-04 15:13:27 +0200

    add code to fix bug

--- palindrome.js
+++ palindrome.js
@@ -8,17 +8,22 @@ function isPalindrome(input_text) {
    let text_lenght = input_text.length
    let mid_text = Math.floor(text_lenght/2);
    
-    for (let i = 0; i < mid; i++) {
+    for (let i = 0; i < mid_text; i++) {
     
     if (input_text[i] == input_text[text_lenght -1 -i] ){
         message = "la parola è palindroma";
+        console.log(message);
+    
         
     }else {
         message = "la parola non è palindroma";
         
+        
     }
  
-   } 
-} 
+ } 
+}
+ 
+
 
 
