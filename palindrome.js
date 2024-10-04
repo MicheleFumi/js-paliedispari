502be9c42db813eb53e9e29e3aa00ab93b700105
MicheleFumi <michele.fumi96@gmail.com>
2024-10-04 15:31:09 +0200

    bug fix for part one

--- palindrome.js
+++ palindrome.js
@@ -2,8 +2,7 @@
 Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
  */
-let message;
-let input_text =prompt("inserisci una parola per verificare la palindricità di essa")
+
 function isPalindrome(input_text) {
    let text_lenght = input_text.length
    let mid_text = Math.floor(text_lenght/2);
@@ -11,18 +10,21 @@ function isPalindrome(input_text) {
     for (let i = 0; i < mid_text; i++) {
     
     if (input_text[i] == input_text[text_lenght -1 -i] ){
-        message = "la parola è palindroma";
-        console.log(message);
-    
+        return "la parola è palindroma";
+        
+        
         
     }else {
-        message = "la parola non è palindroma";
+        return "la parola non è palindroma";
         
         
     }
  
  } 
 }
+
+let input_text =prompt("inserisci una parola per verificare la palindricità di essa")
+ console.log(isPalindrome(input_text));
  
 
 
